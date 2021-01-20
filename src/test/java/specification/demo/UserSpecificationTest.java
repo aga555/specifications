b7226fc1dd40664626b1ee5f2d3e629a2c9c514b
akrawczyk <agammkrawczyk@gmail.com>
2021-01-20 09:35:45 +0100

    naming tests code format

--- src/test/java/specification/demo/UserSpecificationTest.java
+++ src/test/java/specification/demo/UserSpecificationTest.java
@@ -14,19 +14,20 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UserSpecificationTest {
+
     @Autowired
     private UserRepositorySpec userRepositorySpec;
+
     User user;
     User user2;
 
     @BeforeEach
-    void setUp() {
+    void setUpUsers() {
         user = new User();
         user.setEmail("agaakot@gmail.com");
         user.setName("aga");
         user.setLastName("kot");
         userRepositorySpec.save(user);
-
         user2 = new User();
         user2.setEmail("janmak@gmail.com");
         user2.setName("jan");
@@ -38,44 +39,42 @@ class UserSpecificationTest {
     void tearDown() {
         userRepositorySpec.delete(user);
         userRepositorySpec.delete(user2);
-
     }
 
-    @Test
-    void search_with_one_spec_should_work() {
-        List<User> users = userRepositorySpec.findAll(UserSpecification.likeFirstName("aga"));
-        assertNotNull(users);
-        assertEquals(1, users.size());
-    }
 
     @Test
-    void search_with_all_spec_should_work() {
+    void shouldReturnListOfUsersWithSpecicNameLastNameAndEmail() {
         Specification<User> specs = Specification.where(UserSpecification.likeFirstName("aga"))
                 .and(UserSpecification.likeLastName("ot"))
                 .and(UserSpecification.equalEmail("agaakot@gmail.com"));
+
         List<User> users = userRepositorySpec.findAll(specs);
+
         assertNotNull(users);
         assertEquals(1, users.size());
     }
 
     @Test
-    void likeFirstName() {
+    void shouldReturnListOfUsersWithSpecificFirstName() {
         List<User> users = userRepositorySpec.findAll(UserSpecification.likeFirstName("aga"));
+
         assertNotNull(users);
         assertEquals(1, users.size());
 
     }
 
     @Test
-    void likeLastName() {
+    void shouldReturnListOfUsersWithSpeciLastName() {
         List<User> users = userRepositorySpec.findAll(UserSpecification.likeLastName("mak"));
+
         assertNotNull(users);
         assertEquals(1, users.size());
     }
 
     @Test
-    void equalEmail() {
+    void shouldReturnListOfUsersWithSpeciEmail() {
         List<User> users = userRepositorySpec.findAll(UserSpecification.equalEmail("janmak@gmail.com"));
+
         assertNotNull(users);
         assertEquals(1, users.size());
     }
